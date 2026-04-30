3e23c15021e513d1f79054c988a78aa141bbb6a1
kahar-design <kawtar.amrha@gmail.com>
2026-04-30 15:41:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const cards = document.querySelectorAll(".card");
 
+window.addEventListener("scroll", () => {
+  cards.forEach(card => {
+    const position = card.getBoundingClientRect().top;
+    if(position < window.innerHeight - 50){
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
